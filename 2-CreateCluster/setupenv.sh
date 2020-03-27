561d6711153440307b497e72dafbc134666636ed
jsharma19 <jitendra.19@gmail.com>
2020-03-27 12:52:51 +0530

    First commit

---
+++ 2-CreateCluster/setupenv.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+
+export API_TOKEN=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceApiToken')
+export PAAS_TOKEN=$(cat ../1-Credentials/creds.json | jq -r '.dynatracePaaSToken')
+export TENANTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceTenantID')
+export ENVIRONMENTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceEnvironmentID')
+
+
+if hash gcloud 2>/dev/null; then
+    echo "Google Cloud"
+    export CLOUD_PROVIDER=GKE
+elif hash az 2>/dev/null; then
+    echo "Azure Cloud"
+    export CLOUD_PROVIDER=AKS
+else
+    echo "No supported Cloud Provider (GCP or AKS) detected."
+    exit 1;
+fi
+
+echo "Creating $CLOUD_PROVIDER Cluster with the following credentials: "
+echo "API_TOKEN = $API_TOKEN"
+echo "PAAS_TOKEN = $PAAS_TOKEN"
+echo "TENANTID = $TENANTID"
+echo "ENVIRONMENTID = $ENVIRONMENTID"
+echo "Cloud Provider $CLOUD_PROVIDER"
+
+echo ""
+read -p "Is this all correct? (y/n) : " -n 1 -r
+echo ""
+
+usage()
+{
+    echo 'Usage : ./setupenv.sh API_TOKEN PAAS_TOKEN TENANTID ENVIRONMENTID (optional if a SaaS deployment)'
+    exit
+}
+
+deployGKE()
+{
+    echo "Creating GKE Cluster..."
+
+    gcloud container clusters create acmworkshop --zone=us-central1-a --num-nodes=1 --machine-type=n1-highmem-2 --image-type=Ubuntu
+
+    kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+}
+
+deployAKS()
+{
+    echo "Creating AKS Cluster..."
+    export AKS_RESOURCE_GROUP=ACM
+    export AKS_CLUSTER_NAME=acmworkshop
+
+    az group create --name $AKS_RESOURCE_GROUP --location centralus
+    az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --node-count 1 --node-vm-size Standard_B4ms --generate-ssh-keys
+
+    az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+}
+
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    case $CLOUD_PROVIDER in
+        GKE)
+        deployGKE
+        ;;
+        AKS)
+        deployAKS
+        ;;
+        *)
+        echo "No supported Cloud Provider (GCP or AKS) detected."
+        exit 1
+        ;;
+    esac
+else
+    exit 1
+fi
+
+
+echo "Cluster created"
+
+echo "Deploying OneAgent Operator"
+
+../utils/deploy-dt-operator.sh
+
+echo "Waiting for OneAgent to startup..."
+sleep 120
+
+echo "Deploying SockShop Application"
+../utils/deploy-sockshop.sh
+
+sleep 120
+
+echo "Start Production Load"
+nohup ../utils/cartsLoadTest.sh &
+
+echo "Deployment Complete"
+
+
---
+++ 3-DeployJenkins/deployJenkins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+kubectl create -f ../manifests/jenkins/k8s-jenkins-ns.yaml
+kubectl create -f ../manifests/jenkins/k8s-jenkins-pvcs.yaml
+kubectl create -f ../manifests/jenkins/k8s-jenkins-deployment.yaml
+kubectl create -f ../manifests/jenkins/k8s-jenkins-rbac.yaml
+kubectl create -f ../manifests/jenkins/k8s-jenkins-secret.yaml
+
+echo "Waiting for Jenkins to start..."
+
+sleep 120
+
+export JENKINS_URL=$(kubectl describe svc jenkins -n cicd | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
+export JENKINS_URL_PORT=24711
+export JENKINS_USERNAME=$(kubectl get secret jenkins-secret -n cicd -o yaml | grep "username:" | sed 's~username:[ \t]*~~')
+export JENKINS_USERNAME_DECODE=$(echo $JENKINS_USERNAME | base64 --decode)
+export JENKINS_PASSWORD=$(kubectl get secret jenkins-secret -n cicd -o yaml | grep "password:" | sed 's~password:[ \t]*~~')
+export JENKINS_PASSWORD_DECODE=$(echo $JENKINS_PASSWORD | base64 --decode)
+export K8S_MASTER=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " " | sed 's/https\?:\/\///')
+export POD_IP=$(kubectl get pods -n cicd -o yaml | grep "podIP" | sed 's~podIP:[ /t]*~~' | sed -e 's/^[ \t]*//')
+export JENKINS_POD=$(kubectl get po -n cicd --no-headers=true -o custom-columns=:metadata.name)
+
+kubectl exec -it -n cicd $JENKINS_POD -- sed -i "s/JENKINS_SERVER_PLACEHOLDER/https:\/\/$K8S_MASTER/g" /var/jenkins_home/config.xml
+kubectl exec -it -n cicd $JENKINS_POD -- sed -i "s/JENKINS_URL_PLACEHOLDER/http:\/\/$POD_IP:8080/g" /var/jenkins_home/config.xml
+
+curl -s -XPOST http://$JENKINS_URL:$JENKINS_URL_PORT/createItem?name=DeploySockShop -u $JENKINS_USERNAME_DECODE:$JENKINS_PASSWORD_DECODE --data-binary @config.xml -H "Content-Type:text/xml"
+
+#Restart Jenkins
+curl -X POST http://$JENKINS_URL:$JENKINS_URL_PORT/restart -u $JENKINS_USERNAME_DECODE:$JENKINS_PASSWORD_DECODE
+
+#Wait for Jenkins to restart
+sleep 60
+
+echo "----------------------------------------------------"
+echo "Jenkins is running at : http://$JENKINS_URL:$JENKINS_URL_PORT"
+echo "Username is :" $(echo $JENKINS_USERNAME | base64 --decode)
+echo "Password is :" $(echo $JENKINS_PASSWORD | base64 --decode)
+#echo "Kubernetes Master URL :" "https://"$K8S_MASTER""
+#echo "Jenkins URL :" "http://"$POD_IP":8080"
+echo "----------------------------------------------------"
